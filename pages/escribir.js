b14c3dee7dc741e2f1b809602abac0037e8856d5
arcodez <abelileitor123@gmail.com>
2021-03-23 11:39:53 -0430

    add other write effect library

--- pages/escribir.js
+++ pages/escribir.js
@@ -1,20 +1,27 @@
 import Typewriter from "typewriter-effect";
+import Typist from "react-typist";
 
 function escribir() {
   return (
-    <>
-    <title>Efecto de Escritura</title>
+    <div className="text-effect" style={{ textAlign: "center" }}>
+      <title>Efecto de Escritura</title>
+      <h1>Efecto de Escritura</h1>
+      <h2>Primer Ejemplo</h2>
       <Typewriter
         onInit={(typewriter) => {
           typewriter
-          .pauseFor(2500)
-          .typeString('A simple yet powerful native javascript')
-          .pauseFor(300)
-          .deleteChars(10)
-          .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-          .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-          .pauseFor(1000)
-          .start();
+            .pauseFor(2500)
+            .typeString("A simple yet powerful native javascript")
+            .pauseFor(300)
+            .deleteChars(10)
+            .typeString(
+              "<strong>JS</strong> plugin for a cool typewriter effect and "
+            )
+            .typeString(
+              '<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>'
+            )
+            .pauseFor(1000)
+            .start();
         }}
       />
       <Typewriter
@@ -24,8 +31,21 @@ function escribir() {
           loop: true,
         }}
       />
-      <h2>Efecto de Escritura</h2>
-    </>
+      <h2>Segundo Ejemplo</h2>
+      <Typist>
+        <span> First Sentence </span>
+        <br />
+        <div>
+          <p> This will be animated after first sentence is complete </p>
+        </div>
+        Final sentence
+      </Typist>
+      <Typist>
+        <span> First Sentence </span>
+        <Typist.Backspace count={8} delay={200} />
+        <span> Phrase </span>
+      </Typist>
+    </div>
   );
 }
 export default escribir;
