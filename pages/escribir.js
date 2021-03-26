8b357fa1b5355f77739fece677af9c1accfb0dc1
arcodez <abelileitor123@gmail.com>
2021-03-26 11:42:28 -0430

    update write effect

--- pages/escribir.js
+++ pages/escribir.js
@@ -1,4 +1,3 @@
-import Typewriter from "typewriter-effect";
 import Typist from "react-typist";
 
 function escribir() {
@@ -7,30 +6,6 @@ function escribir() {
       <title>Efecto de Escritura</title>
       <h1>Efecto de Escritura</h1>
       <h2>Primer Ejemplo</h2>
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
-            .pauseFor(2500)
-            .typeString("A simple yet powerful native javascript")
-            .pauseFor(300)
-            .deleteChars(10)
-            .typeString(
-              "<strong>JS</strong> plugin for a cool typewriter effect and "
-            )
-            .typeString(
-              '<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>'
-            )
-            .pauseFor(1000)
-            .start();
-        }}
-      />
-      <Typewriter
-        options={{
-          strings: ["Hello", "World"],
-          autoStart: true,
-          loop: true,
-        }}
-      />
       <h2>Segundo Ejemplo</h2>
       <Typist>
         <span> First Sentence </span>
