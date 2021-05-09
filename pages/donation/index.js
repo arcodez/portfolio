ff608103cf9cf0c7738bcf6e528fe6a4bc4f9546
arcodez <abelileitor123@gmail.com>
2021-05-09 19:22:14 -0430

    add paypal html but i have to transform to react

--- pages/donation/index.js
+++ pages/donation/index.js
@@ -5,20 +5,104 @@ function index() {
   const router = useRouter();
   useEffect(() => {
     if (router.pathname === "/donation") {
-      //alert("Es verdadero");
+      //Implementar Layout de Donation
+      alert("Es verdadero")
     }
   }, []);
   return (
     <>
-      <div
-        style={{
-          color: "red",
-        }}
-      >
-        <h1>Hola Cara de Chola</h1>
-        <h1>Hola Chao</h1>
-        <h1>Hola Mundo</h1>
+     <div id="smart-button-container">
+      <div style="text-align: center;">
+        <div style="margin-bottom: 1.25rem;">
+          <p>Doname para poder seguir programando y algun dia poder cumplir mi sueño de Salir de esta mielda de país, Maduro Coño e Tu Madre</p>
+          <select id="item-options"><option value="¡Gracias!" price="5">¡Gracias! - 5 USD</option><option value="¡Arigatou Gosaimasu!" price="10">¡Arigatou Gosaimasu! - 10 USD</option><option value="!Hoy se Come! " price="20">!Hoy se Come!  - 20 USD</option></select>
+          <select style="visibility: hidden" id="quantitySelect"></select>
+        </div>
+      <div id="paypal-button-container"></div>
       </div>
+    </div>
+    <script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD" data-sdk-integration-source="button-factory"></script>
+    <script>
+      function initPayPalButton() {
+        var shipping = 0;
+        var itemOptions = document.querySelector("#smart-button-container #item-options");
+    var quantity = parseInt();
+    var quantitySelect = document.querySelector("#smart-button-container #quantitySelect");
+    if (!isNaN(quantity)) {
+      quantitySelect.style.visibility = "visible";
+    }
+    var orderDescription = 'Doname para poder seguir programando y algun dia poder cumplir mi sueño de Salir de esta mielda de país, Maduro Coño e Tu Madre';
+    if(orderDescription === '') {
+      orderDescription = 'Item';
+    }
+    paypal.Buttons({
+      style: {
+        shape: 'rect',
+        color: 'blue',
+        layout: 'vertical',
+        label: 'paypal',
+        
+      },
+      createOrder: function(data, actions) {
+        var selectedItemDescription = itemOptions.options[itemOptions.selectedIndex].value;
+        var selectedItemPrice = parseFloat(itemOptions.options[itemOptions.selectedIndex].getAttribute("price"));
+        var tax = (0 === 0) ? 0 : (selectedItemPrice * (parseFloat(0)/100));
+        if(quantitySelect.options.length > 0) {
+          quantity = parseInt(quantitySelect.options[quantitySelect.selectedIndex].value);
+        } else {
+          quantity = 1;
+        }
+
+        tax *= quantity;
+        tax = Math.round(tax * 100) / 100;
+        var priceTotal = quantity * selectedItemPrice + parseFloat(shipping) + tax;
+        priceTotal = Math.round(priceTotal * 100) / 100;
+        var itemTotalValue = Math.round((selectedItemPrice * quantity) * 100) / 100;
+
+        return actions.order.create({
+          purchase_units: [{
+            description: orderDescription,
+            amount: {
+              currency_code: 'USD',
+              value: priceTotal,
+              breakdown: {
+                item_total: {
+                  currency_code: 'USD',
+                  value: itemTotalValue,
+                },
+                shipping: {
+                  currency_code: 'USD',
+                  value: shipping,
+                },
+                tax_total: {
+                  currency_code: 'USD',
+                  value: tax,
+                }
+              }
+            },
+            items: [{
+              name: selectedItemDescription,
+              unit_amount: {
+                currency_code: 'USD',
+                value: selectedItemPrice,
+              },
+              quantity: quantity
+            }]
+          }]
+        });
+      },
+      onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+          alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
+      },
+      onError: function(err) {
+        console.log(err);
+      },
+    }).render('#paypal-button-container');
+  }
+  initPayPalButton();
+    </script>
     </>
   );
 }
